bf65f1ba251c0cc135fec605aa96f429324950bb
JusteR <just.ereg@gmail.com>
2014-12-04 17:17:27 +1000

    Выдача прав на папку для группы 1С и включение туда пользователя апач
    Signed-off-by: JusteR <just.ereg@gmail.com>

--- apache22centos6/container-run.sh
+++ apache22centos6/container-run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-chown apache:apache -R /1cconf
-chown apache:apache -R /1c
+# apache user to 1c group
+usermod -a -G grp1cv8 apache
+# files access
+chown apache:grp1cv8 -R /1cconf
+chmod 0770 -R /1cconf
+chown apache:grp1cv8 -R /1c
+chmod 0770 -R /1c
 httpd -DFOREGROUND
 
